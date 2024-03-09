051a59b8775259b2106db4ff076f25aae2186adc
Kyle-Anthony Hay <haykyle917@gmail.com>
2024-03-08 23:59:01 -0500

    Initial Commit

---
+++ ColorfulConnect/ColorfulConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorfulConnectApp.swift
+//  ColorfulConnect
+//
+//  Created by Kyle-Anthony Hay on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorfulConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
